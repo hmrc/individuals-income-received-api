d99189c64d93e7a0926cf237ec8892697a006df1
“ngangaNjiraini” <52919747+ngangaNjiraini@users.noreply.github.com>
2020-06-08 12:34:28 +0100

    MTDSA-5664 Added Response Model for Amend Insurance Policies

---
+++ app/v1/models/response/amendInsurancePolicies/AmendInsurancePoliciesResponce.scala
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.models.response.amendInsurancePolicies
+
+import config.AppConfig
+import v1.hateoas.{HateoasLinks, HateoasLinksFactory}
+import v1.models.hateoas.{HateoasData, Link}
+
+
+case class AmendInsurancePoliciesResponce (responseData: String)
+
+object AmendInsurancePoliciesResponse extends HateoasLinks {
+
+  implicit object AmendInsurancePoliciesLinksFactory extends HateoasLinksFactory[Unit, AmendInsurancePoliciesHateoasData] {
+    override def links(appConfig: AppConfig, data: AmendInsurancePoliciesHateoasData): Seq[Link] = {
+      import data._
+      Seq(
+        amendInsurancePolicies(appConfig, nino, taxYear),
+        retrieveInsurancePolicies(appConfig, nino, taxYear),
+        deleteInsurancePolicies(appConfig, nino, taxYear)
+      )
+    }
+  }
+}
+case class AmendInsurancePoliciesHateoasData(nino: String, taxYear: String) extends HateoasData
