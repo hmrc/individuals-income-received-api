4043233c6d56a348fd4e890442cc266e2eb53520
“Taiwo-AB” <63714619+Taiwo-AB@users.noreply.github.com>
2020-06-24 15:34:04 +0100

    MTDSA-5992 Request models for amend other income endpoint

---
+++ app/v1/models/request/amendOther/AmendAllOtherIncomeReceivedWhilstAbroadItem.scala
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.models.request.amendOther
+
+import play.api.libs.functional.syntax._
+import play.api.libs.json.{JsPath, Json, OWrites, Reads}
+
+case class AmendAllOtherIncomeReceivedWhilstAbroadItem(countryCode: String,
+                                                       amountBeforeTax: Option[BigDecimal],
+                                                       taxTakenOff: Option[BigDecimal],
+                                                       specialWithholdingTax: Option[BigDecimal],
+                                                       foreignTaxCreditRelief: Boolean,
+                                                       taxableAmount: BigDecimal,
+                                                       residentialFinancialCostAmount: Option[BigDecimal],
+                                                       broughtFwdResidentialFinancialCostAmount: Option[BigDecimal])
+
+object AmendAllOtherIncomeReceivedWhilstAbroadItem {
+  implicit val reads: Reads[AmendAllOtherIncomeReceivedWhilstAbroadItem] = Json.reads[AmendAllOtherIncomeReceivedWhilstAbroadItem]
+
+  implicit val writes: OWrites[AmendAllOtherIncomeReceivedWhilstAbroadItem] = (
+    (JsPath \ "countryCode").write[String] and
+      (JsPath \ "amountBeforeTax").writeNullable[BigDecimal] and
+      (JsPath \ "taxTakenOff").writeNullable[BigDecimal] and
+      (JsPath \ "specialWithholdingTax").writeNullable[BigDecimal] and
+      (JsPath \ "foreignTaxCreditRelief").write[Boolean] and
+      (JsPath \ "taxableAmount").write[BigDecimal] and
+      (JsPath \ "residentialFinancialCostAmount").writeNullable[BigDecimal] and
+      (JsPath \ "broughtFwdResidentialFinancialCostAmount").writeNullable[BigDecimal]
+    ) (unlift(AmendAllOtherIncomeReceivedWhilstAbroadItem.unapply))
+}
