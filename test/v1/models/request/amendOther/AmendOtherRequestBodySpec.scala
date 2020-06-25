53e305315797927eef9740bcde68acd6f8172626
Taiwo-AB <63714619+Taiwo-AB@users.noreply.github.com>
2020-06-25 09:12:23 +0100

    MTDSA-5992 Request models for amend other income endpoint (#74)

---
+++ test/v1/models/request/amendOther/AmendOtherRequestBodySpec.scala
@@ -0,0 +1,186 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.models.request.amendOther
+
+import play.api.libs.json.{JsError, JsObject, Json}
+import support.UnitSpec
+
+class AmendOtherRequestBodySpec extends UnitSpec {
+
+  private val json = Json.parse(
+    """
+      |{
+      |   "businessReceipts": [
+      |      {
+      |         "grossAmount": 5000.99,
+      |         "taxYear": "2018-19"
+      |      },
+      |      {
+      |         "grossAmount": 6000.99,
+      |         "taxYear": "2019-20"
+      |      }
+      |   ],
+      |   "allOtherIncomeReceivedWhilstAbroad": [
+      |      {
+      |         "countryCode": "FRA",
+      |         "amountBeforeTax": 1999.99,
+      |         "taxTakenOff": 2.23,
+      |         "specialWithholdingTax": 3.23,
+      |         "foreignTaxCreditRelief": false,
+      |         "taxableAmount": 4.23,
+      |         "residentialFinancialCostAmount": 2999.99,
+      |         "broughtFwdResidentialFinancialCostAmount": 1999.99
+      |      },
+      |      {
+      |         "countryCode": "IND",
+      |         "amountBeforeTax": 2999.99,
+      |         "taxTakenOff": 3.23,
+      |         "specialWithholdingTax": 4.23,
+      |         "foreignTaxCreditRelief": true,
+      |         "taxableAmount": 5.23,
+      |         "residentialFinancialCostAmount": 3999.99,
+      |         "broughtFwdResidentialFinancialCostAmount": 2999.99
+      |      }
+      |   ],
+      |   "overseasIncomeAndGains": {
+      |      "gainAmount": 3000.99
+      |   },
+      |   "chargeableForeignBenefitsAndGifts": {
+      |      "transactionBenefit": 1999.99,
+      |      "protectedForeignIncomeSourceBenefit": 2999.99,
+      |      "protectedForeignIncomeOnwardGift": 3999.99,
+      |      "benefitReceivedAsASettler": 4999.99,
+      |      "onwardGiftReceivedAsASettler": 5999.99
+      |   },
+      |   "omittedForeignIncome": {
+      |      "amount": 4000.99
+      |   }
+      |}
+    """.stripMargin
+  )
+
+  private val businessReceiptsModel = Seq(
+    AmendBusinessReceiptsItem(
+      grossAmount = 5000.99,
+      taxYear = "2018-19"
+    ),
+    AmendBusinessReceiptsItem(
+      grossAmount = 6000.99,
+      taxYear = "2019-20"
+    )
+  )
+
+  private val allOtherIncomeReceivedWhilstAbroadModel = Seq(
+    AmendAllOtherIncomeReceivedWhilstAbroadItem(
+      countryCode = "FRA",
+      amountBeforeTax = Some(1999.99),
+      taxTakenOff = Some(2.23),
+      specialWithholdingTax = Some(3.23),
+      foreignTaxCreditRelief = false,
+      taxableAmount = 4.23,
+      residentialFinancialCostAmount = Some(2999.99),
+      broughtFwdResidentialFinancialCostAmount = Some(1999.99)
+    ),
+    AmendAllOtherIncomeReceivedWhilstAbroadItem(
+      countryCode = "IND",
+      amountBeforeTax = Some(2999.99),
+      taxTakenOff = Some(3.23),
+      specialWithholdingTax = Some(4.23),
+      foreignTaxCreditRelief = true,
+      taxableAmount = 5.23,
+      residentialFinancialCostAmount = Some(3999.99),
+      broughtFwdResidentialFinancialCostAmount = Some(2999.99)
+    )
+  )
+
+  private val overseasIncomeAndGainsModel = AmendOverseasIncomeAndGains(gainAmount = 3000.99)
+
+  private val chargeableForeignBenefitsAndGiftsModel = AmendChargeableForeignBenefitsAndGifts(
+    transactionBenefit = Some(1999.99),
+    protectedForeignIncomeSourceBenefit = Some(2999.99),
+    protectedForeignIncomeOnwardGift = Some(3999.99),
+    benefitReceivedAsASettler = Some(4999.99),
+    onwardGiftReceivedAsASettler = Some(5999.99)
+  )
+
+  private val omittedForeignIncomeModel = AmendOmittedForeignIncome(amount = 4000.99)
+
+  private val requestBodyModel = AmendOtherRequestBody(
+    Some(businessReceiptsModel),
+    Some(allOtherIncomeReceivedWhilstAbroadModel),
+    Some(overseasIncomeAndGainsModel),
+    Some(chargeableForeignBenefitsAndGiftsModel),
+    Some(omittedForeignIncomeModel)
+  )
+
+  "AmendOtherRequestBody" when {
+    "read from valid JSON" should {
+      "produce the expected AmendOtherRequestBody object" in {
+        json.as[AmendOtherRequestBody] shouldBe requestBodyModel
+      }
+    }
+
+    "read from valid JSON with empty chargeableForeignBenefitsAndGifts object, businessReceipts and allOtherIncomeReceivedWhilstAbroad arrays" should {
+      "produce an empty AmendOtherRequestBody object" in {
+        val json = Json.parse(
+          """
+            |{
+            |   "businessReceipts": [ ],
+            |   "allOtherIncomeReceivedWhilstAbroad": [ ],
+            |   "chargeableForeignBenefitsAndGifts": { }
+            |}
+          """.stripMargin
+        )
+
+        json.as[AmendOtherRequestBody] shouldBe AmendOtherRequestBody.empty
+      }
+    }
+
+    "read from empty JSON" should {
+      "produce an empty AmendOtherRequestBody object" in {
+        val emptyJson = JsObject.empty
+
+        emptyJson.as[AmendOtherRequestBody] shouldBe AmendOtherRequestBody.empty
+      }
+    }
+
+    "read from invalid JSON" should {
+      "produce a JsError" in {
+        val invalidJson = Json.parse(
+          """
+            |{
+            |   "businessReceipts": [
+            |      {
+            |         "grossAmount": 5000.99,
+            |         "taxYear": 2018
+            |      }
+            |   ]
+            |}
+          """.stripMargin
+        )
+
+        invalidJson.validate[AmendOtherRequestBody] shouldBe a[JsError]
+      }
+    }
+
+    "written to JSON" should {
+      "produce the expected JsObject" in {
+        Json.toJson(requestBodyModel) shouldBe json
+      }
+    }
+  }
+}
