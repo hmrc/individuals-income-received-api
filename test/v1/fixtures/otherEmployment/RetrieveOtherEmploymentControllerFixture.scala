6095006d1dc9f930f31ec67be1c6157efb302d51
dwalters <32767569+david-walters-HMRC@users.noreply.github.com>
2020-07-02 08:17:53 +0100

    MTDSA-6045 added controller for retrieve other employment endpoint

---
+++ test/v1/fixtures/otherEmployment/RetrieveOtherEmploymentControllerFixture.scala
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.fixtures.otherEmployment
+
+import play.api.libs.json.{JsObject, JsValue, Json}
+
+object RetrieveOtherEmploymentControllerFixture {
+
+  val mtdResponse: JsValue = Json.parse(
+    """
+      |{
+      | "shareOption":[
+      |   {
+      |      "employerName": "Company Ltd",
+      |      "employerRef" : "AB1321/123",
+      |      "schemePlanType": "EMI",
+      |      "dateOfOptionGrant": "2019-11-20",
+      |      "dateOfEvent": "2019-12-22",
+      |      "optionNotExercisedButConsiderationReceived": true,
+      |      "amountOfConsiderationReceived": 23122.22,
+      |      "noOfSharesAcquired": 1,
+      |      "classOfSharesAcquired": "FIRST",
+      |      "exercisePrice": 12.22,
+      |      "amountPaidForOption": 123.22,
+      |      "marketValueOfSharesOnExcise": 1232.22,
+      |      "profitOnOptionExercised": 1232.33,
+      |      "employersNicPaid": 2312.22,
+      |      "taxableAmount" : 2132.22
+      |   }
+      | ],
+      | "sharesAwardedOrReceived":[
+      |   {
+      |     "employerName": "Company Ltd",
+      |     "employerRef" : "AB1321/123",
+      |     "schemePlanType": "SIP",
+      |     "dateSharesCeasedToBeSubjectToPlan": "2019-11-10",
+      |     "noOfShareSecuritiesAwarded": 11,
+      |     "classOfShareAwarded": "FIRST",
+      |     "dateSharesAwarded" : "2019-12-20",
+      |     "sharesSubjectToRestrictions": true,
+      |     "electionEnteredIgnoreRestrictions": false,
+      |     "actualMarketValueOfSharesOnAward": 2123.22,
+      |     "unrestrictedMarketValueOfSharesOnAward": 123.22,
+      |     "amountPaidForSharesOnAward": 123.22,
+      |     "marketValueAfterRestrictionsLifted": 1232.22,
+      |     "taxableAmount": 12321.22
+      |   }
+      | ],
+      | "disability":{
+      |   "customerReference": "customer reference",
+      |   "amountDeducted": 1223.22
+      | },
+      | "foreignService":{
+      |   "customerReference": "customer reference",
+      |   "amountDeducted": 1223.22
+      | }
+      |}
+      |""".stripMargin
+  )
+
+  def mtdResponseWithHateoas(nino: String, taxYear: String): JsObject = mtdResponse.as[JsObject] ++ Json.parse(
+    s"""
+       |{
+       |   "links":[
+       |      {
+       |         "href":"/individuals/income-received/employments/other/$nino/$taxYear",
+       |         "method":"PUT",
+       |         "rel":"amend-employments-other-income"
+       |      },
+       |      {
+       |         "href":"/individuals/income-received/employments/other/$nino/$taxYear",
+       |         "method":"GET",
+       |         "rel":"self"
+       |      },
+       |      {
+       |         "href":"/individuals/income-received/employments/other/$nino/$taxYear",
+       |         "method":"DELETE",
+       |         "rel":"delete-employments-other-income"
+       |      }
+       |   ]
+       |}
+    """.stripMargin
+  ).as[JsObject]
+}
