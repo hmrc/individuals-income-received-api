44c070fb67b00e89e59f120cb9ee0410b2ea9d73
simon <55548012+simon1552@users.noreply.github.com>
2020-07-03 13:31:16 +0100

    renamed file to match the test spec

--- test/v1/models/request/amendDividends/AmendCommonDividendSpec.scala
+++ test/v1/models/request/amendDividends/AmendCommonDividendSpec.scala
@@ -19,7 +19,7 @@ package v1.models.request.amendDividends
 import play.api.libs.json.{JsError, JsObject, Json}
 import support.UnitSpec
 
-class AmendStockDividendSpec extends UnitSpec {
+class AmendCommonDividendSpec extends UnitSpec {
 
   private val json = Json.parse(
     """
