94b0202e0aed17463435401b99b4d5e9e898bd88
“Taiwo-AB” <63714619+Taiwo-AB@users.noreply.github.com>
2020-07-16 16:07:12 +0100

    MTDSA-6373 Request Parser and Validator for Delete Custom Employment endpoint
    MTDSA-6373 Request Parser and Validator for Delete Custom Employment endpoint
    
    MTDSA-6373 file name change
    
    MTDSA-6373 Tax year fixes
    
    MTDSA-6373 remove unused imports
    
    MTDSA-6373 Updated error messages as pec Spec

---
+++ app/v1/controllers/requestParsers/validators/DeleteCustomEmploymentValidator.scala
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.controllers.requestParsers.validators
+
+import v1.controllers.requestParsers.validators.validations._
+import v1.models.errors.{MtdError, RuleTaxYearNotSupportedError}
+import v1.models.request.deleteCustomEmployment.DeleteCustomEmploymentRawData
+
+class DeleteCustomEmploymentValidator extends Validator[DeleteCustomEmploymentRawData] {
+
+  private val validationSet = List(parameterFormatValidation, parameterRuleValidation)
+
+  override def validate(data: DeleteCustomEmploymentRawData): List[MtdError] = {
+    run(validationSet, data).distinct
+  }
+
+  private def parameterFormatValidation: DeleteCustomEmploymentRawData => List[List[MtdError]] = (data: DeleteCustomEmploymentRawData) => {
+    List(
+      NinoValidation.validate(data.nino),
+      TaxYearValidation.validate(data.taxYear),
+      EmploymentIdValidation.validate(data.employmentId)
+    )
+  }
+
+  private def parameterRuleValidation: DeleteCustomEmploymentRawData => List[List[MtdError]] = (data: DeleteCustomEmploymentRawData) => {
+    List(
+      MtdTaxYearValidation.validate(data.taxYear, RuleTaxYearNotSupportedError)
+    )
+  }
+}
