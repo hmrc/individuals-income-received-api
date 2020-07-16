61c49107f00448af7230ffdd14575f7815a95a05
simon <55548012+simon1552@users.noreply.github.com>
2020-07-16 15:21:46 +0100

    Draft 2

--- test/v1/models/request/amendCustomEmployments/AmendCustomEmploymentRequestBodySpec.scala
+++ test/v1/models/request/amendCustomEmployments/AmendCustomEmploymentRequestBodySpec.scala
@@ -30,6 +30,98 @@ class AmendCustomEmploymentRequestBodySpec extends UnitSpec {
       payrollId = Some("124214112412")
     )
 
+  private val desResponse: JsValue = Json.parse(
+    """
+      |{
+      |  "employerRef": "123/AZ12334",
+      |  "employerName": "AMD infotech Ltd",
+      |  "startDate": "2019-01-01",
+      |  "cessationDate": "2020-06-01",
+      |  "payrollId": "124214112412"
+      |}
+    """.stripMargin
+  )
+
+  private val minimumModel = AmendCustomEmploymentRequestBody(
+    employerRef = None,
+    employerName = "AMD infotech Ltd",
+    startDate = "2019-01-01",
+    cessationDate = None,
+    payrollId = None
+  )
+
+  private val mtdResponse: JsValue = Json.parse(
+    """
+      |{
+      |  "employerRef": "123/AZ12334",
+      |  "employerName": "AMD infotech Ltd",
+      |  "startDate": "2019-01-01",
+      |  "cessationDate": "2020-06-01",
+      |  "payrollId": "124214112412"
+      |}
+    """.stripMargin
+  )
+
+  private val desResponseInvalid: JsValue = Json.parse(
+    """
+      |{
+      |  "employerRef": 123.12334,
+      |  "employerName": "#123ad",
+      |  "startDate": "abc",
+      |  "cessationDate": 120.22,
+      |  "payrollId": 124214112412
+      |}
+    """.stripMargin
+  )
+
+  private val minimumDesResponse: JsValue = Json.parse(
+    """
+      |{
+      |  "employerName": "AMD infotech Ltd",
+      |  "startDate": "2019-01-01"
+      |}
+    """.stripMargin
+  )
+
+  private val minimumMtdResponse: JsValue = Json.parse(
+    """
+      |{
+      |  "employerName": "AMD infotech Ltd",
+      |  "startDate": "2019-01-01"
+      |}
+    """.stripMargin
+  )
+
+  "CustomEmploymentItem" when {
+    "read from valid JSON" should {
+      "produce the expected CustomEmploymentItem object" in {
+        desResponse.as[AmendCustomEmploymentRequestBody] shouldBe model
+      }
+    }
+
+    "read from a JSON with only mandatory fields" should {
+      "produce the expected CustomEmploymentItem object" in {
+        minimumDesResponse.as[AmendCustomEmploymentRequestBody] shouldBe minimumModel
+      }
+    }
+
+    "read from invalid JSON" should {
+      "produce a JsError" in {
+        desResponseInvalid.validate[AmendCustomEmploymentRequestBody] shouldBe a[JsError]
+      }
+    }
+
+    "written to JSON" should {
+      "produce the expected JSON object" in {
+        Json.toJson(model) shouldBe mtdResponse
+      }
+    }
+
+    "not write empty fields" in {
+      Json.toJson(minimumModel) shouldBe minimumMtdResponse
+    }
+  }
+
   private val json = Json.parse(
     """
       |{
