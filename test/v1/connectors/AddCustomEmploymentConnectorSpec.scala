14c9a79d8197771827d2ba536c8b21e8e09a1e4c
MarkAKelly <mark.kelly@digital.hmrc.gov.uk>
2020-07-16 12:42:00 +0100

    [MTDSA-6395] add connector for Add Custom Employment

---
+++ test/v1/connectors/AddCustomEmploymentConnectorSpec.scala
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.connectors
+
+import mocks.MockAppConfig
+import uk.gov.hmrc.domain.Nino
+import v1.mocks.MockHttpClient
+import v1.models.domain.DesTaxYear
+import v1.models.outcomes.ResponseWrapper
+import v1.models.request.addCustomEmployment.{AddCustomEmploymentRequest, AddCustomEmploymentRequestBody}
+
+import scala.concurrent.Future
+
+class AddCustomEmploymentConnectorSpec extends ConnectorSpec {
+
+  val nino: String = "AA111111A"
+  val taxYear: String = "2021-22"
+
+  val amendCustomEmploymentRequestBody: AddCustomEmploymentRequestBody = AddCustomEmploymentRequestBody(
+    employerRef = Some("123/AB56797"),
+    employerName = "AMD infotech Ltd",
+    startDate = "2019-01-01",
+    cessationDate = Some("2020-06-01"),
+    payrollId = Some("124214112412")
+  )
+
+  val request: AddCustomEmploymentRequest = AddCustomEmploymentRequest(
+    nino = Nino(nino),
+    taxYear = DesTaxYear(taxYear),
+    body = amendCustomEmploymentRequestBody
+  )
+
+  class Test extends MockHttpClient with MockAppConfig {
+
+    val connector: AddCustomEmploymentConnector = new AddCustomEmploymentConnector(
+      http = mockHttpClient,
+      appConfig = mockAppConfig
+    )
+
+    MockedAppConfig.desBaseUrl returns baseUrl
+    MockedAppConfig.desToken returns "des-token"
+    MockedAppConfig.desEnvironment returns "des-environment"
+  }
+
+  "AddCustomEmploymentConnector" when {
+    ".addEmployment" should {
+      "return a success upon HttpClient success" in new Test {
+        val outcome = Right(ResponseWrapper(correlationId, ()))
+
+        MockedHttpClient
+          .post(
+            url = s"$baseUrl/income-tax/income/employments/$nino/$taxYear/custom",
+            body = amendCustomEmploymentRequestBody,
+            requiredHeaders = "Environment" -> "des-environment", "Authorization" -> s"Bearer des-token"
+          ).returns(Future.successful(outcome))
+
+        await(connector.addEmployment(request)) shouldBe outcome
+      }
+    }
+  }
+}
