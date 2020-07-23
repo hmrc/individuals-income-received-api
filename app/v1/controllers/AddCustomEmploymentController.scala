6bc2856d1ecc3b21cf8da23e7e883916ba1b1303
lukejones <47660116+BetaDraconis@users.noreply.github.com>
2020-07-23 12:05:39 +0100

    MTDSA-6398: Add controller, and route for 'add custom employment'

---
+++ app/v1/controllers/AddCustomEmploymentController.scala
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.controllers
+
+import cats.data.EitherT
+import cats.implicits._
+import config.AppConfig
+import javax.inject.Inject
+import play.api.libs.json.{JsValue, Json}
+import play.api.mvc.{Action, AnyContentAsJson, ControllerComponents}
+import play.mvc.Http.MimeTypes
+import utils.Logging
+import v1.controllers.requestParsers.AddCustomEmploymentRequestParser
+import v1.hateoas.AmendHateoasBody
+import v1.models.errors._
+import v1.models.request.addCustomEmployment.AddCustomEmploymentRawData
+import v1.services.{AddCustomEmploymentService, EnrolmentsAuthService, MtdIdLookupService}
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class AddCustomEmploymentController @Inject()(val authService: EnrolmentsAuthService,
+                                              val lookupService: MtdIdLookupService,
+                                              appConfig: AppConfig,
+                                              requestParser: AddCustomEmploymentRequestParser,
+                                              service: AddCustomEmploymentService,
+                                              cc: ControllerComponents)(implicit ec: ExecutionContext)
+  extends AuthorisedController(cc) with BaseController with Logging with AmendHateoasBody {
+
+  implicit val endpointLogContext: EndpointLogContext =
+    EndpointLogContext(
+      controllerName = "AddCustomEmploymentController",
+      endpointName = "addEmployment"
+    )
+
+  def addEmployment(nino: String, taxYear: String): Action[JsValue] =
+    authorisedAction(nino).async(parse.json) { implicit request =>
+
+      val rawData: AddCustomEmploymentRawData = AddCustomEmploymentRawData(
+        nino = nino,
+        taxYear = taxYear,
+        body = AnyContentAsJson(request.body)
+      )
+
+      val result =
+        for {
+          parsedRequest <- EitherT.fromEither[Future](requestParser.parseRequest(rawData))
+          serviceResponse <- EitherT(service.addEmployment(parsedRequest))
+        } yield {
+          logger.info(
+            s"[${endpointLogContext.controllerName}][${endpointLogContext.endpointName}] - " +
+              s"Success response received with CorrelationId: ${serviceResponse.correlationId}")
+
+          Ok(amendOtherEmploymentHateoasBody(appConfig, nino, taxYear))
+            .withApiHeaders(serviceResponse.correlationId)
+            .as(MimeTypes.JSON)
+        }
+
+      result.leftMap { errorWrapper =>
+        val correlationId = getCorrelationId(errorWrapper)
+        val result = errorResult(errorWrapper).withApiHeaders(correlationId)
+
+        result
+      }.merge
+    }
+
+  private def errorResult(errorWrapper: ErrorWrapper) = {
+    (errorWrapper.error: @unchecked) match {
+      case BadRequestError | NinoFormatError | TaxYearFormatError | RuleIncorrectOrEmptyBodyError |
+           RuleTaxYearRangeInvalidError | RuleTaxYearNotSupportedError | RuleTaxYearNotEndedError |
+           EmployerRefFormatError | EmployerNameFormatError | PayrollIdFormatError |
+           StartDateFormatError | CessationDateFormatError | RuleCessationDateBeforeStartDateError |
+           RuleStartDateAfterTaxYearEndError | RuleCessationDateBeforeTaxYearStartError
+      => BadRequest(Json.toJson(errorWrapper))
+      case NotFoundError => NotFound(Json.toJson(errorWrapper))
+      case DownstreamError => InternalServerError(Json.toJson(errorWrapper))
+    }
+  }
+}
