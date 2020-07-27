f9e54925d6dfeeec780b67a7762250349eeb25b6
lukejones <47660116+BetaDraconis@users.noreply.github.com>
2020-07-27 17:03:16 +0100

    missing-field-poc: update JsonFormatValidation to use RuleIncorrectOrEmptyBodyError

--- app/v1/controllers/requestParsers/validators/validations/JsonFormatValidation.scala
+++ app/v1/controllers/requestParsers/validators/validations/JsonFormatValidation.scala
@@ -16,33 +16,37 @@
 
 package v1.controllers.requestParsers.validators.validations
 
+import play.api.Logger
 import play.api.libs.json._
-import v1.controllers.requestParsers.validators.Validator
-import v1.models.errors.{MissingFieldError, MtdError, WrongFieldTypeError}
+import v1.models.errors.{MtdError, RuleIncorrectOrEmptyBodyError}
 
 object JsonFormatValidation {
 
-  case class PathWrapper(path: JsPath, errorType: MtdError) {
-    override def toString: String = path
-      .toString()
-      .replace("(", "/")
-      .replace(")", "")
-
-    val getError: MtdError = this.errorType.copy(paths = Some(List(this.toString)))
-  }
+  private def fromJsPath(path: JsPath): String = path
+    .toString()
+    .replace("(", "/")
+    .replace(")", "")
 
   def validate[A](data: JsValue, error: MtdError)(implicit reads: Reads[A], writes: Writes[A]): List[MtdError] = {
     if (data == JsObject.empty) List(error) else
       data.validate[A] match {
         case JsSuccess(body, _) =>
           if (Json.toJson(body) == JsObject.empty) List(error) else NoValidationErrors
-        case JsError(errors: Seq[(JsPath, Seq[JsonValidationError])]) => errors.flatMap{
-          case (path: JsPath, Seq(JsonValidationError(Seq("error.path.missing")))) => Some(PathWrapper(path, MissingFieldError))
-          case (path: JsPath, Seq(JsonValidationError(Seq(error: String)))) if error.contains("error.expected") => Some(PathWrapper(path, WrongFieldTypeError))
+        case JsError(errors: Seq[(JsPath, Seq[JsonValidationError])]) => errors.flatMap {
+          case (path: JsPath, Seq(JsonValidationError(Seq("error.path.missing")))) => {
+            Logger.warn(s"[JsonFormatValidation][validate] - " +
+              s"A mandatory field is missing the the request body - $path")
+            Some(fromJsPath(path))
+          }
+          case (path: JsPath, Seq(JsonValidationError(Seq(error: String)))) if error.contains("error.expected") => {
+            Logger.warn(s"[JsonFormatValidation][validate] - " +
+              s"A field has been supplied with the incorrect type - $path")
+            Some(fromJsPath(path))
+          }
           case _ => None
         } match {
           case Nil => List(error)
-          case paths => Validator.flattenErrors(paths.map(path => List(path.getError)).toList)
+          case paths => List(RuleIncorrectOrEmptyBodyError.copy(paths = Some(paths)))
         }
       }
   }
