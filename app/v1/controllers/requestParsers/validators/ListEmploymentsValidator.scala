0535742fe2c294a6b014cb1d916cb7390be6df0b
“Taiwo-AB” <63714619+Taiwo-AB@users.noreply.github.com>
2020-07-28 08:19:21 +0100

    MTDSA-6507 Request Parser and Validator for list employments

---
+++ app/v1/controllers/requestParsers/validators/ListEmploymentsValidator.scala
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.controllers.requestParsers.validators
+
+import config.AppConfig
+import javax.inject.Inject
+import utils.CurrentDateTime
+import v1.controllers.requestParsers.validators.validations._
+import v1.models.errors.MtdError
+import v1.models.request.listEmployments.ListEmploymentsRawData
+
+class ListEmploymentsValidator @Inject()(implicit currentDateTime: CurrentDateTime, appConfig: AppConfig)
+  extends Validator[ListEmploymentsRawData] {
+
+  private val validationSet = List(parameterFormatValidation, parameterRuleValidation)
+
+  override def validate(data: ListEmploymentsRawData): List[MtdError] = {
+    run(validationSet, data).distinct
+  }
+
+  private def parameterFormatValidation: ListEmploymentsRawData => List[List[MtdError]] = (data: ListEmploymentsRawData) => {
+    List(
+      NinoValidation.validate(data.nino),
+      TaxYearValidation.validate(data.taxYear)
+    )
+  }
+
+  private def parameterRuleValidation: ListEmploymentsRawData => List[List[MtdError]] = (data: ListEmploymentsRawData) => {
+    List(
+      MtdTaxYearValidation.validate(data.taxYear)
+    )
+  }
+}
