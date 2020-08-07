1c032b0d7f47c06801f66edc79c4bd584df6fb67
Yang Wong <20066247+yangwong9664@users.noreply.github.com>
2020-08-07 14:39:04 +0100

    MTDSA-6600 delete employment financial details API (#151)
    * delete employtment
    
    * delete employtment
    
    * delete employtment
    
    * update url
    
    * MTDSA-6600 added extra tests

---
+++ test/v1/controllers/DeleteEmploymentFinancialDetailsControllerSpec.scala
@@ -0,0 +1,152 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.controllers
+
+import play.api.libs.json.Json
+import play.api.mvc.Result
+import uk.gov.hmrc.domain.Nino
+import uk.gov.hmrc.http.HeaderCarrier
+import v1.mocks.requestParsers.MockDeleteCustomEmploymentRequestParser
+import v1.mocks.services.{MockDeleteRetrieveService, MockEnrolmentsAuthService, MockMtdIdLookupService}
+import v1.models.errors._
+import v1.models.outcomes.ResponseWrapper
+import v1.models.request.deleteCustomEmployment.{DeleteCustomEmploymentRawData, DeleteCustomEmploymentRequest}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+class DeleteEmploymentFinancialDetailsControllerSpec
+  extends ControllerBaseSpec
+    with MockEnrolmentsAuthService
+    with MockMtdIdLookupService
+    with MockDeleteRetrieveService
+    with MockDeleteCustomEmploymentRequestParser {
+
+  val nino: String = "AA123456A"
+  val taxYear: String = "2019-20"
+  val employmentId: String = "4557ecb5-fd32-48cc-81f5-e6acd1099f3c"
+  val correlationId: String = "X-123"
+
+  val rawData: DeleteCustomEmploymentRawData = DeleteCustomEmploymentRawData(
+    nino = nino,
+    taxYear = taxYear,
+    employmentId = employmentId
+  )
+
+  val requestData: DeleteCustomEmploymentRequest = DeleteCustomEmploymentRequest(
+    nino = Nino(nino),
+    taxYear = taxYear,
+    employmentId = employmentId
+  )
+
+  trait Test {
+    val hc = HeaderCarrier()
+
+    val controller = new DeleteEmploymentFinancialDetailsController(
+      authService = mockEnrolmentsAuthService,
+      lookupService = mockMtdIdLookupService,
+      requestParser = mockDeleteCustomEmploymentRequestParser,
+      service = mockDeleteRetrieveService,
+      cc = cc
+    )
+
+    MockedMtdIdLookupService.lookup(nino).returns(Future.successful(Right("test-mtd-id")))
+    MockedEnrolmentsAuthService.authoriseUser()
+  }
+
+  "deleteEmploymentFinancialDetailsController" should {
+    "return NO_content" when {
+      "happy path" in new Test {
+
+        MockDeleteCustomEmploymentRequestParser
+          .parse(rawData)
+          .returns(Right(requestData))
+
+        MockDeleteRetrieveService
+          .delete()
+          .returns(Future.successful(Right(ResponseWrapper(correlationId, ()))))
+
+        val result: Future[Result] = controller.deleteEmploymentFinancialDetails(nino, taxYear, employmentId)(fakeDeleteRequest)
+
+        status(result) shouldBe NO_CONTENT
+        contentAsString(result) shouldBe ""
+        header("X-CorrelationId", result) shouldBe Some(correlationId)
+      }
+    }
+
+    "return the error as per spec" when {
+      "parser errors occur" must {
+        def errorsFromParserTester(error: MtdError, expectedStatus: Int): Unit = {
+          s"a ${error.code} error is returned from the parser" in new Test {
+
+            MockDeleteCustomEmploymentRequestParser
+              .parse(rawData)
+              .returns(Left(ErrorWrapper(Some(correlationId), error, None)))
+
+            val result: Future[Result] = controller.deleteEmploymentFinancialDetails(nino, taxYear, employmentId)(fakeDeleteRequest)
+
+            status(result) shouldBe expectedStatus
+            contentAsJson(result) shouldBe Json.toJson(error)
+            header("X-CorrelationId", result) shouldBe Some(correlationId)
+          }
+        }
+
+        val input = Seq(
+          (BadRequestError, BAD_REQUEST),
+          (NinoFormatError, BAD_REQUEST),
+          (TaxYearFormatError, BAD_REQUEST),
+          (EmploymentIdFormatError, BAD_REQUEST),
+          (RuleTaxYearNotSupportedError, BAD_REQUEST),
+          (RuleTaxYearRangeInvalidError, BAD_REQUEST)
+        )
+
+        input.foreach(args => (errorsFromParserTester _).tupled(args))
+      }
+
+      "service errors occur" must {
+        def serviceErrors(mtdError: MtdError, expectedStatus: Int): Unit = {
+          s"a $mtdError error is returned from the service" in new Test {
+
+            MockDeleteCustomEmploymentRequestParser
+              .parse(rawData)
+              .returns(Right(requestData))
+
+            MockDeleteRetrieveService
+              .delete()
+              .returns(Future.successful(Left(ErrorWrapper(Some(correlationId), mtdError))))
+
+            val result: Future[Result] = controller.deleteEmploymentFinancialDetails(nino, taxYear, employmentId)(fakeDeleteRequest)
+
+            status(result) shouldBe expectedStatus
+            contentAsJson(result) shouldBe Json.toJson(mtdError)
+            header("X-CorrelationId", result) shouldBe Some(correlationId)
+          }
+        }
+
+        val input = Seq(
+          (NinoFormatError, BAD_REQUEST),
+          (TaxYearFormatError, BAD_REQUEST),
+          (EmploymentIdFormatError, BAD_REQUEST),
+          (NotFoundError, NOT_FOUND),
+          (DownstreamError, INTERNAL_SERVER_ERROR)
+        )
+
+        input.foreach(args => (serviceErrors _).tupled(args))
+      }
+    }
+  }
+}
