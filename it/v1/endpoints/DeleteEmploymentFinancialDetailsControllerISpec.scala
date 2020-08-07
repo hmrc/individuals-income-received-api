4b3f691d0bdca59349ec80c7fb3ccb766f4951ec
yang <20066247+yangwong9664@users.noreply.github.com>
2020-08-07 13:26:01 +0100

    MTDSA-6600 added extra tests

--- it/v1/endpoints/DeleteEmploymentFinancialDetailsControllerISpec.scala
+++ it/v1/endpoints/DeleteEmploymentFinancialDetailsControllerISpec.scala
@@ -92,7 +92,9 @@ class DeleteEmploymentFinancialDetailsControllerISpec extends IntegrationBaseSpe
           ("AA1123A", "2019-20", "4557ecb5-fd32-48cc-81f5-e6acd1099f3c", BAD_REQUEST, NinoFormatError),
           ("AA123456A", "20122", "78d9f015-a8b4-47a8-8bbc-c253a1e8057e", BAD_REQUEST, TaxYearFormatError),
           ("AA123456A", "2019-20", "ABCDE12345FG", BAD_REQUEST, EmploymentIdFormatError),
-          ("AA123456A", "2019-21", "4557ecb5-fd32-48cc-81f5-e6acd1099f3c", BAD_REQUEST, RuleTaxYearRangeInvalidError))
+          ("AA123456A", "2019-21", "4557ecb5-fd32-48cc-81f5-e6acd1099f3c", BAD_REQUEST, RuleTaxYearRangeInvalidError),
+          ("AA123456A", "2016-17", "4557ecb5-fd32-48cc-81f5-e6acd1099f3c", BAD_REQUEST, RuleTaxYearNotSupportedError)
+        )
 
         input.foreach(args => (validationErrorTest _).tupled(args))
       }
