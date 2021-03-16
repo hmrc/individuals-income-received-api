5a3fdbb0202c130c422504c8a56cb6716e501163
lukejones <47660116+BetaDraconis@users.noreply.github.com>
2021-03-16 11:14:00 +0000

    MTDSA-8449: Update config to include IFS details.

--- app/config/AppConfig.scala
+++ app/config/AppConfig.scala
@@ -26,19 +26,18 @@ trait AppConfig {
   def mtdIdBaseUrl: String
 
   def desBaseUrl: String
-
   def desEnv: String
-
   def desToken: String
 
-  def apiGatewayContext: String
+  def ifsBaseUrl: String
+  def ifsEnv: String
+  def ifsToken: String
 
+  def apiGatewayContext: String
   def minimumPermittedTaxYear: Int
 
   def apiStatus(version: String): String
-
   def featureSwitch: Option[Configuration]
-
   def endpointsEnabled(version: String): Boolean
 
   def confidenceLevelConfig: ConfidenceLevelConfig
@@ -48,16 +47,20 @@ trait AppConfig {
 class AppConfigImpl @Inject()(config: ServicesConfig, configuration: Configuration) extends AppConfig {
 
   val mtdIdBaseUrl: String = config.baseUrl("mtd-id-lookup")
+
   val desBaseUrl: String = config.baseUrl("des")
   val desEnv: String = config.getString("microservice.services.des.env")
   val desToken: String = config.getString("microservice.services.des.token")
+
+  val ifsBaseUrl: String = config.baseUrl("ifs")
+  val ifsEnv: String = config.getString("microservice.services.ifs.env")
+  val ifsToken: String = config.getString("microservice.services.ifs.token")
+
   val apiGatewayContext: String = config.getString("api.gateway.context")
   val minimumPermittedTaxYear: Int = config.getInt("minimumPermittedTaxYear")
 
   def apiStatus(version: String): String = config.getString(s"api.$version.status")
-
   def featureSwitch: Option[Configuration] = configuration.getOptional[Configuration](s"feature-switch")
-
   def endpointsEnabled(version: String): Boolean = config.getBoolean(s"api.$version.endpoints.enabled")
 
   val confidenceLevelConfig: ConfidenceLevelConfig = configuration.get[ConfidenceLevelConfig](s"api.confidence-level-check")
--- test/mocks/MockAppConfig.scala
+++ test/mocks/MockAppConfig.scala
@@ -26,25 +26,18 @@ trait MockAppConfig extends MockFactory {
   val mockAppConfig: AppConfig = mock[AppConfig]
 
   object MockedAppConfig {
-
     def desBaseUrl: CallHandler[String] = (mockAppConfig.desBaseUrl _: () => String).expects()
-
     def desToken: CallHandler[String] = (mockAppConfig.desToken _).expects()
-
     def desEnvironment: CallHandler[String] = (mockAppConfig.desEnv _).expects()
-
+    def ifsBaseUrl: CallHandler[String] = (mockAppConfig.ifsBaseUrl _: () => String).expects()
+    def ifsToken: CallHandler[String] = (mockAppConfig.ifsToken _).expects()
+    def ifsEnvironment: CallHandler[String] = (mockAppConfig.ifsEnv _).expects()
     def mtdIdBaseUrl: CallHandler[String] = (mockAppConfig.mtdIdBaseUrl _: () => String).expects()
-
     def featureSwitch: CallHandler[Option[Configuration]] = (mockAppConfig.featureSwitch _: () => Option[Configuration]).expects()
-
-    def apiGatewayContext: CallHandler[String]            = (mockAppConfig.apiGatewayContext _: () => String).expects()
-
+    def apiGatewayContext: CallHandler[String] = (mockAppConfig.apiGatewayContext _: () => String).expects()
     def apiStatus: CallHandler[String] = (mockAppConfig.apiStatus: String => String).expects("1.0")
-
     def endpointsEnabled: CallHandler[Boolean] = (mockAppConfig.endpointsEnabled: String => Boolean).expects("1.0")
-
     def minimumPermittedTaxYear: CallHandler[Int] = (mockAppConfig.minimumPermittedTaxYear _).expects()
-
     def confidenceLevelCheckEnabled: CallHandler[ConfidenceLevelConfig] = (mockAppConfig.confidenceLevelConfig _: () => ConfidenceLevelConfig).expects()
   }
 }
