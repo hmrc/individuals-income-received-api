d6ea9b4517a3838525ad4799714be5f5182bc943
James Whiteley <31282758+james-work-account@users.noreply.github.com>
2021-04-08 11:07:18 +0100

    [MTDSA-8655] update dependencies (#227)

--- it/v1/endpoints/IgnoreEmploymentControllerISpec.scala
+++ it/v1/endpoints/IgnoreEmploymentControllerISpec.scala
@@ -112,7 +112,7 @@ class IgnoreEmploymentControllerISpec extends IntegrationBaseSpec {
           ("AA123456A", "2019-20", "ABCDE12345FG", BAD_REQUEST, EmploymentIdFormatError, None),
           ("AA123456A", "2018-19", "78d9f015-a8b4-47a8-8bbc-c253a1e8057e", BAD_REQUEST, RuleTaxYearNotSupportedError, None),
           ("AA123456A", "2019-21", "4557ecb5-fd32-48cc-81f5-e6acd1099f3c", BAD_REQUEST, RuleTaxYearRangeInvalidError, None),
-          ("AA123456A", "2020-21", "78d9f015-a8b4-47a8-8bbc-c253a1e8057e", BAD_REQUEST, RuleTaxYearNotEndedError, None)
+          ("AA123456A", "2021-22", "78d9f015-a8b4-47a8-8bbc-c253a1e8057e", BAD_REQUEST, RuleTaxYearNotEndedError, None)
         )
 
         input.foreach(args => (validationErrorTest _).tupled(args))
