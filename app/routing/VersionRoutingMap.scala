31681cfb1a3a852b4c5ccf355b6a9532511a99c5
vfryer12 <72739659+vfryer12@users.noreply.github.com>
2021-04-14 11:24:12 +0100

    MTDSA-8695 Feature switch off for Foreign Income Endpoints. (#229)

--- app/routing/VersionRoutingMap.scala
+++ app/routing/VersionRoutingMap.scala
@@ -17,6 +17,7 @@
 package routing
 
 import com.google.inject.ImplementedBy
+import config.{AppConfig, FeatureSwitch}
 import definition.Versions.VERSION_1
 import javax.inject.Inject
 import play.api.routing.Router
@@ -34,9 +35,16 @@ trait VersionRoutingMap {
 }
 
 // Add routes corresponding to available versions...
-case class VersionRoutingMapImpl @Inject()(defaultRouter: Router, v1Router: v1.Routes) extends VersionRoutingMap {
+case class VersionRoutingMapImpl @Inject()(appConfig: AppConfig,
+                                           defaultRouter: Router,
+                                           v1Router: v1.Routes,
+                                           liveRouter: live.Routes) extends VersionRoutingMap {
+
+  val featureSwitch: FeatureSwitch = FeatureSwitch(appConfig.featureSwitch)
 
   val map: Map[String, Router] = Map(
-    VERSION_1 -> v1Router
+    VERSION_1 -> {
+      if (featureSwitch.isFullRoutingEnabled) v1Router else liveRouter
+    }
   )
 }
