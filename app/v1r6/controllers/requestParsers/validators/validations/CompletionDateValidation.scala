54cae0cccf9fdbf3c06919ecac48875518356d36
jeremystone <6453521+jeremystone@users.noreply.github.com>
2021-09-07 14:56:57 +0100

    MTDSA-9561 - fix a couple of edge cases

--- app/v1r6/controllers/requestParsers/validators/validations/CompletionDateValidation.scala
+++ app/v1r6/controllers/requestParsers/validators/validations/CompletionDateValidation.scala
@@ -16,13 +16,13 @@
 
 package v1r6.controllers.requestParsers.validators.validations
 
+import org.joda.time.DateTime
+import org.joda.time.format.DateTimeFormat
 import utils.CurrentDateTime
 import v1r6.models.domain.DesTaxYear
 import v1r6.models.errors.{MtdError, RuleCompletionDateError}
-import java.time.LocalDate
 
-import org.joda.time.DateTime
-import org.joda.time.format.DateTimeFormat
+import java.time.LocalDate
 
 object CompletionDateValidation {
   private val MARCH = 3
@@ -46,7 +46,7 @@ object CompletionDateValidation {
 
     val dateIsBefore7thMarch = formattedDate.isBefore(march7th)
     val dateIsAfterToday = formattedDate.isAfter(currentDateTime.getLocalDate)
-    val dateIsInTaxYear = formattedDate.isAfter(fromDate) && formattedDate.isBefore(toDate)
+    val dateIsInTaxYear = !formattedDate.isBefore(fromDate) && !formattedDate.isAfter(toDate)
     val taxYearIsCurrentTaxYear = DesTaxYear.fromMtd(taxYear).value.toInt == currentTaxYear
 
     if(dateIsBefore7thMarch && taxYearIsCurrentTaxYear || dateIsAfterToday || !dateIsInTaxYear) {
