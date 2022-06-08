8cb14ff9e5b858c21317e4283e4e233b2dd03c94
Sean Flynn <106095575+sean-flynn-HMRC@users.noreply.github.com>
2022-06-08 13:26:27 +0100

    Request parser complete

---
+++ app/v1/requestParsers/validators/CreateAmendUKDividendsIncomeAnnualSummaryValidator.scala
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.requestParsers.validators
+
+import api.models.errors.MtdError
+import api.requestParsers.validators.Validator
+import config.AppConfig
+import utils.CurrentDateTime
+import v1.requestParsers.validators.validations._
+import v1.models.request.createAmendNonPayeEmployment._
+import v1.requestParsers.validators.validations.{JsonFormatValidation, NinoValidation, TaxYearNotEndedValidation, TaxYearNotSupportedValidation, TaxYearValidation, TipsValidation}
+
+import javax.inject.{Inject, Singleton}
+
+@Singleton
+class CreateAmendUKDividendsIncomeAnnualSummaryValidator @Inject()(implicit currentDateTime: CurrentDateTime, appConfig: AppConfig)
+  extends Validator[CreateAmendNonPayeEmploymentRawData] {
+
+  private val validationSet = List(
+    parameterFormatValidation,
+    parameterRuleValidation,
+    bodyFormatValidator,
+    bodyValueValidator
+  )
+
+  override def validate(data: CreateAmendUKDividendsIncomeAnnualSalaryRawData): List[MtdError] = {
+    run(validationSet, data).distinct
+  }
+
+  private def parameterFormatValidation: CreateAmendUKDividendsINcomeAnnualSalaryRawData => List[List[MtdError]] = data => {
+    List(
+      NinoValidation.validate(data.nino),
+      TaxYearValidation.validate(data.taxYear)
+    )
+  }
+
+  private def parameterRuleValidation: CreateAmendUKDividendsINcomeAnnualSalaryRawData  => List[List[MtdError]] = data => {
+    List(
+      TaxYearNotSupportedValidation.validate(data.taxYear, appConfig.minimumPermittedTaxYear),
+      TaxYearNotEndedValidation.validate(data.taxYear)
+    )
+  }
+
+  private def bodyFormatValidator: CreateAmendUKDividendsINcomeAnnualSalaryRawData  => List[List[MtdError]] = { data =>
+    List(
+      JsonFormatValidation.validate[CreateAmendNonPayeEmploymentRequestBody](data.body.json)
+    )
+  }
+
+  private def bodyValueValidator: CreateAmendNonPayeEmploymentRawData => List[List[MtdError]] = { data =>
+    val requestBody = data.body.json.as[CreateAmendNonPayeEmploymentRequestBody]
+
+    List(
+      TipsValidation.validateWithPath(
+        amount = requestBody.tips,
+        path = s"/"
+      )
+    )
+  }
+
+}
