c6a954bfc86dca099ea02c55958f7e7d9eed5e82
DreamcastKnight <56401347+DreamcastKnight@users.noreply.github.com>
2022-06-09 13:33:27 +0100

    addressed comment

--- app/v1/models/request/deleteUkDividendsIncomeAnnualSummary/DeleteUkDividendsIncomeAnnualSummaryRequest.scala
+++ app/v1/models/request/deleteUkDividendsIncomeAnnualSummary/DeleteUkDividendsIncomeAnnualSummaryRequest.scala
@@ -16,6 +16,6 @@
 
 package v1.models.request.deleteUkDividendsIncomeAnnualSummary
 
-import api.models.domain.Nino
+import api.models.domain.{DesTaxYear, Nino}
 
-case class DeleteUkDividendsIncomeAnnualSummaryRequest(nino: Nino, taxYear: String)
+case class DeleteUkDividendsIncomeAnnualSummaryRequest(nino: Nino, taxYear: DesTaxYear)
