641054ac3d8ca58272299095b4ed1d16d45bfecd
DreamcastKnight <56401347+DreamcastKnight@users.noreply.github.com>
2022-06-15 14:54:36 +0100

    started on retrieve controller

---
+++ app/v1/controllers/RetrieveUkDividendsAnnualIncomeSummaryController.scala
@@ -0,0 +1,112 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.controllers
+
+import api.connectors.DownstreamUri.IfsUri
+import api.controllers.{AuthorisedController, BaseController, EndpointLogContext}
+import api.hateoas.HateoasFactory
+import api.models.errors._
+import api.models.request.DeleteRetrieveRawData
+import api.services.{EnrolmentsAuthService, MtdIdLookupService}
+import cats.data.EitherT
+import cats.implicits.catsSyntaxEitherId
+import javax.inject.{Inject, Singleton}
+import play.api.libs.json.Json
+import play.api.mvc.{Action, AnyContent, ControllerComponents}
+import play.mvc.Http.MimeTypes
+import utils.{IdGenerator, Logging}
+import v1.models.request.retrieveUkDividendsAnnualIncomeSummary.RetrieveUkDividendsAnnualIncomeSummaryRawData
+import v1.models.response.retrieveUkDividendsAnnualIncomeSummary.{RetrieveUkDividendsAnnualIncomeSummaryHateoasData, RetrieveUkDividendsAnnualIncomeSummaryResponse}
+import v1.requestParsers.RetrieveUkDividendsIncomeAnnualSummaryRequestParser
+import v1.services.RetrieveUKDividendsIncomeAnnualSummaryService
+
+import scala.concurrent.{ExecutionContext, Future}
+
+@Singleton
+class RetrieveUkDividendsAnnualIncomeSummaryController @Inject() (val authService: EnrolmentsAuthService,
+                                                                  val lookupService: MtdIdLookupService,
+                                                                  requestParser: RetrieveUkDividendsIncomeAnnualSummaryRequestParser,
+                                                                  service: RetrieveUKDividendsIncomeAnnualSummaryService,
+                                                                  hateoasFactory: HateoasFactory,
+                                                                  cc: ControllerComponents,
+                                                                  val idGenerator: IdGenerator)(implicit ec: ExecutionContext)
+  extends AuthorisedController(cc)
+    with BaseController
+    with Logging {
+
+  implicit val endpointLogContext: EndpointLogContext =
+    EndpointLogContext(
+      controllerName = "RetrieveUkDividendsAnnualIncomeSummaryController",
+      endpointName = "retrieveUkDividendsAnnualIncomeSummary"
+    )
+
+  def retrieveUkDividends(nino: String, taxYear: String): Action[AnyContent] =
+    authorisedAction(nino).async { implicit request =>
+      implicit val correlationId: String = idGenerator.generateCorrelationId
+      logger.info(
+        s"[${endpointLogContext.controllerName}][${endpointLogContext.endpointName}] " +
+          s"with CorrelationId: $correlationId")
+
+      val rawData: RetrieveUkDividendsAnnualIncomeSummaryRawData = RetrieveUkDividendsAnnualIncomeSummaryRawData(
+        nino = nino,
+        taxYear = taxYear
+      )
+
+      implicit val ifsUri: IfsUri[RetrieveUkDividendsAnnualIncomeSummaryResponse] = IfsUri[RetrieveUkDividendsAnnualIncomeSummaryResponse](
+        s"income-tax/income/dividends/$nino/$taxYear"
+      )
+
+      val result =
+        for {
+          _               <- EitherT.fromEither[Future](requestParser.parseRequest(rawData))
+          serviceResponse <- EitherT(service.retrieveUKDividendsIncomeAnnualSummary[RetrieveUkDividendsAnnualIncomeSummaryResponse]())
+          vendorResponse <- EitherT.fromEither[Future](
+            hateoasFactory
+              .wrap(serviceResponse.responseData,RetrieveUkDividendsAnnualIncomeSummaryHateoasData(nino, taxYear))
+              .asRight[ErrorWrapper])
+        } yield {
+          logger.info(
+            s"[${endpointLogContext.controllerName}][${endpointLogContext.endpointName}] - " +
+              s"Success response received with CorrelationId: ${serviceResponse.correlationId}")
+
+          Ok(Json.toJson(vendorResponse))
+            .withApiHeaders(serviceResponse.correlationId)
+            .as(MimeTypes.JSON)
+        }
+
+      result.leftMap { errorWrapper =>
+        val resCorrelationId = errorWrapper.correlationId
+        val result           = errorResult(errorWrapper).withApiHeaders(resCorrelationId)
+        logger.warn(
+          s"[${endpointLogContext.controllerName}][${endpointLogContext.endpointName}] - " +
+            s"Error response received with CorrelationId: $resCorrelationId")
+
+        result
+      }.merge
+    }
+
+  private def errorResult(errorWrapper: ErrorWrapper) =
+    errorWrapper.error match {
+      case BadRequestError | NinoFormatError | TaxYearFormatError | RuleTaxYearRangeInvalidError | RuleTaxYearNotSupportedError =>
+        BadRequest(Json.toJson(errorWrapper))
+      case NotFoundError           => NotFound(Json.toJson(errorWrapper))
+      case StandardDownstreamError => InternalServerError(Json.toJson(errorWrapper))
+      case _                       => unhandledError(errorWrapper)
+    }
+
+}
+
---
+++ test/v1/controllers/RetrieveUkDividendsAnnualIncomeSummaryControllerSpec.scala
@@ -0,0 +1,197 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.controllers
+
+import api.controllers.ControllerBaseSpec
+import api.hateoas.HateoasLinks
+import api.mocks.MockIdGenerator
+import api.mocks.hateoas.MockHateoasFactory
+import api.mocks.requestParsers.MockDeleteRetrieveRequestParser
+import api.mocks.services.{MockDeleteRetrieveService, MockEnrolmentsAuthService, MockMtdIdLookupService}
+import api.models.domain.Nino
+import api.models.errors.{BadRequestError, ErrorWrapper, MtdError, NinoFormatError, NotFoundError, RuleTaxYearNotSupportedError, RuleTaxYearRangeInvalidError, StandardDownstreamError, TaxYearFormatError}
+import api.models.hateoas.{HateoasWrapper, Link}
+import api.models.hateoas.Method.{DELETE, GET, PUT}
+import api.models.hateoas.RelType.{AMEND_DIVIDENDS_INCOME, DELETE_DIVIDENDS_INCOME, SELF}
+import api.models.outcomes.ResponseWrapper
+import api.models.request
+import api.models.request.{DeleteRetrieveRawData, DeleteRetrieveRequest}
+import play.api.libs.json.Json
+import play.api.mvc.Result
+import uk.gov.hmrc.http.HeaderCarrier
+import v1.models.response.retrieveUkDividendsAnnualIncomeSummary.{RetrieveUkDividendsAnnualIncomeSummaryHateoasData, RetrieveUkDividendsAnnualIncomeSummaryResponse}
+
+import scala.concurrent.Future
+
+class RetrieveUkDividendsAnnualIncomeSummaryControllerSpec
+  extends ControllerBaseSpec
+    with MockEnrolmentsAuthService
+    with MockMtdIdLookupService
+    with MockDeleteRetrieveService
+    with MockHateoasFactory
+    with MockRetrieve
+    with HateoasLinks
+    with MockIdGenerator {
+
+  private val nino: String          = "AA123456A"
+  private val taxYear: String       = "2019-20"
+  private val correlationId: String = "X-123"
+
+  private val rawData: DeleteRetrieveRawData = DeleteRetrieveRawData(
+    nino = nino,
+    taxYear = taxYear
+  )
+
+  private val requestData: DeleteRetrieveRequest = request.DeleteRetrieveRequest(
+    nino = Nino(nino),
+    taxYear = taxYear
+  )
+
+  private val amendUkDividendsLink: Link =
+    Link(
+      href = s"/individuals/income-received/uk-dividends/$nino/$taxYear",
+      method = PUT,
+      rel = AMEND_DIVIDENDS_INCOME
+    )
+
+  private val retrieveUkDividendsLink: Link =
+    Link(
+      href = s"/individuals/income-received/uk-dividends/$nino/$taxYear",
+      method = GET,
+      rel = SELF
+    )
+
+  private val deleteUkDividendsLink: Link =
+    Link(
+      href = s"/individuals/income-received/uk-dividends/$nino/$taxYear",
+      method = DELETE,
+      rel = DELETE_DIVIDENDS_INCOME
+    )
+
+
+  private val mtdResponse = ""
+
+  trait Test {
+    val hc: HeaderCarrier = HeaderCarrier()
+
+    val controller = new RetrieveUkDividendsAnnualIncomeSummaryController()(
+      authService = mockEnrolmentsAuthService,
+      lookupService = mockMtdIdLookupService,
+      requestParser = mockDeleteRetrieveRequestParser,
+      service = mockDeleteRetrieveService,
+      hateoasFactory = mockHateoasFactory,
+      cc = cc,
+      idGenerator = mockIdGenerator
+    )
+
+    MockedMtdIdLookupService.lookup(nino).returns(Future.successful(Right("test-mtd-id")))
+    MockedEnrolmentsAuthService.authoriseUser()
+    MockIdGenerator.generateCorrelationId.returns(correlationId)
+  }
+
+  "RetrieveDividendsController" should {
+    "return OK" when {
+      "happy path" in new Test {
+
+        MockDeleteRetrieveRequestParser
+          .parse(rawData)
+          .returns(Right(requestData))
+
+        MockDeleteRetrieveService
+          .retrieve[RetrieveUkDividendsAnnualIncomeSummaryResponse](defaultDownstreamErrorMap)
+          .returns(Future.successful(Right(ResponseWrapper(correlationId, retrieveUkDividendsAnnualIncomeSummaryResponseModel))))
+
+        MockHateoasFactory
+          .wrap(RetrieveUkDividendsAnnualIncomeSummaryResponseModel, RetrieveUkDividendsAnnualIncomeSummaryHateoasData(nino, taxYear))
+          .returns(
+            HateoasWrapper(
+              retrieveUkDividendsAnnualIncomeSummaryResponseModel,
+              Seq(
+                amendDividendsLink,
+                retrieveDividendsLink,
+                deleteDividendsLink
+              )))
+
+        val result: Future[Result] = controller.retrieveDividends(nino, taxYear)(fakeGetRequest)
+
+        status(result) shouldBe OK
+        contentAsJson(result) shouldBe mtdResponse
+        header("X-CorrelationId", result) shouldBe Some(correlationId)
+      }
+    }
+
+    "return the error as per spec" when {
+      "parser errors occur" must {
+        def errorsFromParserTester(error: MtdError, expectedStatus: Int): Unit = {
+          s"a ${error.code} error is returned from the parser" in new Test {
+
+            MockDeleteRetrieveRequestParser
+              .parse(rawData)
+              .returns(Left(ErrorWrapper(correlationId, error, None)))
+
+            val result: Future[Result] = controller.retrieveDividends(nino, taxYear)(fakeGetRequest)
+
+            status(result) shouldBe expectedStatus
+            contentAsJson(result) shouldBe Json.toJson(error)
+            header("X-CorrelationId", result) shouldBe Some(correlationId)
+          }
+        }
+
+        val input = Seq(
+          (BadRequestError, BAD_REQUEST),
+          (NinoFormatError, BAD_REQUEST),
+          (TaxYearFormatError, BAD_REQUEST),
+          (RuleTaxYearNotSupportedError, BAD_REQUEST),
+          (RuleTaxYearRangeInvalidError, BAD_REQUEST)
+        )
+
+        input.foreach(args => (errorsFromParserTester _).tupled(args))
+      }
+
+      "service errors occur" must {
+        def serviceErrors(mtdError: MtdError, expectedStatus: Int): Unit = {
+          s"a $mtdError error is returned from the service" in new Test {
+
+            MockDeleteRetrieveRequestParser
+              .parse(rawData)
+              .returns(Right(requestData))
+
+            MockDeleteRetrieveService
+              .retrieve[RetrieveUkDividendsAnnualIncomeSummaryResponse](defaultDownstreamErrorMap)
+              .returns(Future.successful(Left(ErrorWrapper(correlationId, mtdError))))
+
+            val result: Future[Result] = controller.retrieveDividends(nino, taxYear)(fakeGetRequest)
+
+            status(result) shouldBe expectedStatus
+            contentAsJson(result) shouldBe Json.toJson(mtdError)
+            header("X-CorrelationId", result) shouldBe Some(correlationId)
+          }
+        }
+
+        val input = Seq(
+          (NinoFormatError, BAD_REQUEST),
+          (TaxYearFormatError, BAD_REQUEST),
+          (NotFoundError, NOT_FOUND),
+          (StandardDownstreamError, INTERNAL_SERVER_ERROR)
+        )
+
+        input.foreach(args => (serviceErrors _).tupled(args))
+      }
+    }
+  }
+
+}
