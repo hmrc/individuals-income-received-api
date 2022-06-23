68de5120e8838becfd16c344719961c1c5b44437
DreamcastKnight <56401347+DreamcastKnight@users.noreply.github.com>
2022-06-23 10:05:00 +0100

    started tests

---
+++ test/v1/models/request/addUkSavings/AddUkSavingsRequestBodySpec.scala
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.models.request.addUkSavings
+
+import play.api.libs.json.{JsError, JsObject, Json}
+import support.UnitSpec
+
+class AddUkSavingsRequestBodySpec extends UnitSpec {
+
+  val mtdJson = Json.parse(
+    """
+      |{
+      |   "accountName": "Shares savings account"
+      |}
+      |""".stripMargin)
+
+  val desJson = Json.parse(
+    """
+      |{
+      |    "incomeSourceType": "interest-from-uk-banks"
+      |}
+      |""".stripMargin)
+
+
+  val model: AddUkSavingsRequestBody = AddUkSavingsRequestBody("Shares savings account")
+
+  "AddUkSavings" when {
+    "read from a valid JSON" should {
+      "produce the expected object" in {
+        desJson.as[AddUkSavingsRequestBody] shouldBe model
+      }
+    }
+  }
+
+  "read from empty JSON" should {
+    "produce a JsError" in {
+      val invalidJson = JsObject.empty
+      invalidJson.validate[AddUkSavingsRequestBody] shouldBe a[JsError]
+    }
+  }
+
+
+
+}
