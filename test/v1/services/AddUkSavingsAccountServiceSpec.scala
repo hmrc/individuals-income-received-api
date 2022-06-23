bdf8350194b31fbeffeb3e24322fadc16e6f2539
Narendra Vijayarao <narendra.vijayarao@digital.hmrc.gov.uk>
2022-06-23 15:50:40 +0100

    Basic conn & service template for Add UK savings

---
+++ test/v1/services/AddUkSavingsAccountServiceSpec.scala
@@ -0,0 +1,97 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.services
+
+import api.controllers.EndpointLogContext
+import api.models.domain.Nino
+import api.models.errors._
+import api.models.outcomes.ResponseWrapper
+import api.services.ServiceSpec
+import v1.mocks.connectors.MockAddUkSavingsAccountConnector
+import v1.models.request.amendSavings.{AmendForeignInterestItem, AmendSavingsRequest, AmendSavingsRequestBody}
+
+import scala.concurrent.Future
+
+class AddUkSavingsAccountServiceSpec extends ServiceSpec {
+
+  private val nino    = "AA112233A"
+  private val taxYear = "2019-20"
+
+  val foreignInterest: AmendForeignInterestItem = AmendForeignInterestItem(
+    amountBeforeTax = None,
+    countryCode = "FRA",
+    taxTakenOff = None,
+    specialWithholdingTax = None,
+    taxableAmount = 233.11,
+    foreignTaxCreditRelief = false
+  )
+
+  private val amendSavingsRequest = AmendSavingsRequest(
+    nino = Nino(nino),
+    taxYear = taxYear,
+    body = AmendSavingsRequestBody(securities = None, foreignInterest = Some(Seq(foreignInterest)))
+  )
+
+  trait Test extends MockAddUkSavingsAccountConnector {
+    implicit val logContext: EndpointLogContext = EndpointLogContext("c", "ep")
+
+    val service: AddUkSavingsAccountService = new AddUkSavingsAccountService(
+      connector = mockAddUkSavingsAccountConnector
+    )
+
+  }
+
+  "AmendSavingsService" when {
+    "amendSaving" must {
+      "return correct result for a success" in new Test {
+        private val outcome = Right(ResponseWrapper(correlationId, ()))
+
+        MockAddUkSavingsAccountConnector
+          .addSavings(amendSavingsRequest)
+          .returns(Future.successful(outcome))
+
+        await(service.addSavings(amendSavingsRequest)) shouldBe outcome
+      }
+
+      "map errors according to spec" when {
+
+        def serviceError(desErrorCode: String, error: MtdError): Unit =
+          s"a $desErrorCode error is returned from the service" in new Test {
+
+            MockAddUkSavingsAccountConnector
+              .addSavings(amendSavingsRequest)
+              .returns(Future.successful(Left(ResponseWrapper(correlationId, DownstreamErrors.single(DownstreamErrorCode(desErrorCode))))))
+
+            await(service.addSavings(amendSavingsRequest)) shouldBe Left(ErrorWrapper(correlationId, error))
+          }
+
+        val input = Seq(
+          ("INVALID_IDVALUE", NinoFormatError),
+          ("MAX_ACCOUNTS_REACHED", RuleMaximumSavingsAccountsLimitError),
+          ("ALREADY_EXISTS", RuleDuplicateAccountNameError),
+          ("INVALID_IDTYPE", StandardDownstreamError),
+          ("INVALID_PAYLOAD", StandardDownstreamError),
+          ("SERVER_ERROR", StandardDownstreamError),
+          ("SERVICE_UNAVAILABLE", StandardDownstreamError)
+        )
+
+        input.foreach(args => (serviceError _).tupled(args))
+      }
+    }
+  }
+
+}
