97f75087e4bd055edea1bc212b3b80b2d55c7b78
sean-flynn-HMRC <106095575+sean-flynn-HMRC@users.noreply.github.com>
2022-06-30 17:29:43 +0100

    Validator begun.

---
+++ app/v1/requestParsers/validators/CreateAmendUkSavingsAccountAnnualSummaryValidator.scala
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.requestParsers.validators
+
+import api.models.errors.MtdError
+import api.requestParsers.validators.Validator
+import config.AppConfig
+import v1.models.request.createAmendUkDividendsIncomeAnnualSummary.{CreateAmendUkDividendsIncomeAnnualSummaryBody, CreateAmendUkDividendsIncomeAnnualSummaryRawData}
+import v1.models.request.createAmendUkSavingsAnnualSummary.{CreateAmendUkSavingsAnnualSummaryBody, CreateAmendUkSavingsAnnualSummaryRawData}
+import v1.requestParsers.validators.validations._
+
+import javax.inject.{Inject, Singleton}
+
+@Singleton
+class CreateAmendUkSavingsAccountAnnualSummaryValidator @Inject() (appConfig: AppConfig)
+  extends Validator[CreateAmendUkSavingsAnnualSummaryRawData] {
+
+  private val validationSet = List(
+    parameterFormatValidation,
+    parameterRuleValidation,
+    bodyFormatValidator,
+    bodyValueValidator
+  )
+
+  override def validate(data: CreateAmendUkSavingsAnnualSummaryRawData): List[MtdError] = {
+    run(validationSet, data).distinct
+  }
+
+  private def parameterFormatValidation: CreateAmendUkSavingsAnnualSummaryRawData => List[List[MtdError]] = data => {
+    List(
+      NinoValidation.validate(data.nino),
+      TaxYearValidation.validate(data.taxYear)
+    )
+  }
+
+  private def parameterRuleValidation: CreateAmendUkSavingsAnnualSummaryRawData => List[List[MtdError]] = data => {
+    List(
+      TaxYearNotSupportedValidation.validate(data.taxYear, appConfig.minimumPermittedTaxYear)
+    )
+  }
+
+  private def bodyFormatValidator: CreateAmendUkSavingsAnnualSummaryRawData => List[List[MtdError]] = { data =>
+    List(
+      JsonFormatValidation.validate[CreateAmendUkSavingsAnnualSummaryBody](data.body.json)
+    )
+
+  }
+
+  private def bodyValueValidator: CreateAmendUkSavingsAnnualSummaryRawData => List[List[MtdError]] = { data =>
+    val requestBody = data.body.json.as[CreateAmendUkSavingsAnnualSummaryBody]
+
+    List(
+      //Validation goes here.
+    )
+
+  }
+
+}
