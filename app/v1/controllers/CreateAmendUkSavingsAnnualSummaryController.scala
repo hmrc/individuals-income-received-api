bb0f591c84f6c0e3b782dbccfe30050b102c4f25
jeremystone <6453521+jeremystone@users.noreply.github.com>
2022-07-01 12:10:05 +0100

    MTDSA-12117 - create/amend uk savings controller - tests and mocks

---
+++ app/v1/controllers/CreateAmendUkSavingsAnnualSummaryController.scala
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.controllers
+
+import api.controllers.{AuthorisedController, BaseController, EndpointLogContext}
+import api.hateoas.{AmendHateoasBody, HateoasFactory}
+import api.models.errors._
+import api.services.{EnrolmentsAuthService, MtdIdLookupService}
+import cats.data.EitherT
+import cats.implicits._
+import play.api.libs.json.{JsValue, Json}
+import play.api.mvc.{Action, AnyContentAsJson, ControllerComponents}
+import utils.{IdGenerator, Logging}
+import v1.models.request.createAmendUkSavingsAnnualSummary.CreateAmendUkSavingsAnnualSummaryRawData
+import v1.models.response.createAmendUkSavingsIncomeAnnualSummary.CreateAndAmendUkSavingsAnnualSummaryHateoasData
+import v1.requestParsers.CreateAmendUkSavingsAnnualSummaryRequestParser
+import v1.services.CreateAmendUkSavingsAnnualSummaryService
+
+import javax.inject.{Inject, Singleton}
+import scala.concurrent.{ExecutionContext, Future}
+
+@Singleton
+class CreateAmendUkSavingsAnnualSummaryController @Inject() (val authService: EnrolmentsAuthService,
+                                                             val lookupService: MtdIdLookupService,
+                                                             requestParser: CreateAmendUkSavingsAnnualSummaryRequestParser,
+                                                             service: CreateAmendUkSavingsAnnualSummaryService,
+                                                             hateoasFactory: HateoasFactory,
+                                                             cc: ControllerComponents,
+                                                             val idGenerator: IdGenerator)(implicit ec: ExecutionContext)
+    extends AuthorisedController(cc)
+    with BaseController
+    with Logging
+    with AmendHateoasBody {
+
+  implicit val endpointLogContext: EndpointLogContext =
+    EndpointLogContext(
+      controllerName = "CreateAmendUkSavingsAnnualSummaryController",
+      endpointName = "createAmendUkSavingsAnnualSummary"
+    )
+
+  def createAmendUkSavingsAnnualSummary(nino: String, taxYear: String, savingsAccountId: String): Action[JsValue] =
+    authorisedAction(nino).async(parse.json) { implicit request =>
+      implicit val correlationId: String = idGenerator.generateCorrelationId
+      logger.info(
+        s"[${endpointLogContext.controllerName}][${endpointLogContext.endpointName}]" +
+          s"with CorrelationId: $correlationId")
+
+      val rawData = CreateAmendUkSavingsAnnualSummaryRawData(
+        nino = nino,
+        taxYear = taxYear,
+        savingsAccountId = savingsAccountId,
+        body = AnyContentAsJson(request.body)
+      )
+
+      val result = for {
+        parsedRequest   <- EitherT.fromEither[Future](requestParser.parseRequest(rawData))
+        serviceResponse <- EitherT(service.createAmend(parsedRequest))
+        vendorResponse <- EitherT.fromEither[Future](
+          hateoasFactory
+            .wrap((), CreateAndAmendUkSavingsAnnualSummaryHateoasData(nino, taxYear, savingsAccountId))
+            .asRight[ErrorWrapper])
+      } yield {
+        logger.info(
+          s"[${endpointLogContext.controllerName}][${endpointLogContext.endpointName}] - " +
+            s"Success response received with CorrelationId: ${serviceResponse.correlationId}"
+        )
+
+        Ok(Json.toJson(vendorResponse))
+          .withApiHeaders(serviceResponse.correlationId)
+      }
+
+      result.leftMap { errorWrapper =>
+        val resCorrelationId = errorWrapper.correlationId
+        logger.warn(
+          s"[${endpointLogContext.controllerName}][${endpointLogContext.endpointName}] " +
+            s"Error response received with CorrelationId: $resCorrelationId")
+
+        errorResult(errorWrapper).withApiHeaders(resCorrelationId)
+      }.merge
+    }
+
+  private def errorResult(errorWrapper: ErrorWrapper) =
+    errorWrapper.error match {
+      case BadRequestError | NinoFormatError | SavingsAccountIdFormatError | TaxYearFormatError | RuleTaxYearRangeInvalidError |
+          RuleTaxYearNotSupportedError | CustomMtdError(ValueFormatError.code) | CustomMtdError(RuleIncorrectOrEmptyBodyError.code) =>
+        BadRequest(Json.toJson(errorWrapper))
+      case NotFoundError           => NotFound(Json.toJson(errorWrapper))
+      case StandardDownstreamError => InternalServerError(Json.toJson(errorWrapper))
+      case _                       => unhandledError(errorWrapper)
+    }
+
+}
