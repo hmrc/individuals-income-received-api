a7bdc1fbd61f55ecf5ad0cf36b8eedb2bc0fe34c
sean-flynn-HMRC <106095575+sean-flynn-HMRC@users.noreply.github.com>
2022-07-05 16:57:28 +0100

    Started Service and Connector

---
+++ app/v1/connectors/ListUkSavingsAccountsConnector.scala
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.connectors
+
+import api.connectors.BaseDownstreamConnector
+import config.AppConfig
+
+import javax.inject.{Inject, Singleton}
+import uk.gov.hmrc.http.{HeaderCarrier, HttpClient}
+import api.connectors.DownstreamUri.Release6Uri
+import v1.models.request.listEmployments.ListEmploymentsRequest
+import v1.models.response.listEmployment.{Employment, ListEmploymentResponse}
+
+import scala.concurrent.{ExecutionContext, Future}
+import api.connectors.{BaseDownstreamConnector, DownstreamOutcome}
+
+@Singleton
+class ListUkSavingsAccountsConnector @Inject() (val http: HttpClient, val appConfig: AppConfig) extends BaseDownstreamConnector {
+
+  // Wait for Models to be completed, then ListUkSavingsRequest will get resolved.
+  def listUkSavingsAccounts(request: ListUkSavingsRequest)(implicit
+      hc: HeaderCarrier,
+      ec: ExecutionContext,
+      correlationId: String): Future[DownstreamOutcome[ListEmploymentResponse[Employment]]] = {
+
+    import api.connectors.httpparsers.StandardDownstreamHttpParser._
+
+    val nino    = request.nino.nino
+    val taxYear = request.taxYear
+
+    get(
+      Release6Uri[ListEmploymentResponse[Employment]](s"income-tax/income/employments/$nino/$taxYear")
+    )
+  }
+
+}
