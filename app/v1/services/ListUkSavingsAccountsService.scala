cfa7f81dad56e23e27e777cc4cdf4ea5062103ed
sean-flynn-HMRC <106095575+sean-flynn-HMRC@users.noreply.github.com>
2022-07-12 08:07:09 +0100

    [MTDSA 12175] Connector + Service for List All Savings Accounts (#427)
    * Started Service and Connector
    
    * First draft of connector, service and tests
    
    * [MTDSA-12175] switch to DES config as per the PR comment
    
    * [MTDSA-12175] Fix the test and production code to use the correct backend uri as per the PR comment
    
    * [MTDSA-12175] Fix the test and production code to pass incomeSourceId conditionally
    
    Co-authored-by: Naree Song <107855324+naree-hmrc@users.noreply.github.com>

---
+++ app/v1/services/ListUkSavingsAccountsService.scala
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.services
+
+import api.controllers.EndpointLogContext
+import api.models.errors.{ErrorWrapper, MtdError, NinoFormatError, NotFoundError, SavingsAccountIdFormatError, StandardDownstreamError}
+import api.models.outcomes.ResponseWrapper
+import api.support.DownstreamResponseMappingSupport
+import cats.data.EitherT
+import cats.implicits._
+
+import javax.inject.{Inject, Singleton}
+import uk.gov.hmrc.http.HeaderCarrier
+import utils.Logging
+import v1.connectors.ListUkSavingsAccountsConnector
+import v1.models.request.listUkSavingsAccount.ListUkSavingsAccountRequest
+import v1.models.response.listUkSavingsAccount.{ListUkSavingsAccountResponse, UkSavingsAccount}
+
+import scala.concurrent.{ExecutionContext, Future}
+
+@Singleton
+class ListUkSavingsAccountsService @Inject() (connector: ListUkSavingsAccountsConnector) extends DownstreamResponseMappingSupport with Logging {
+
+  def listUkSavingsAccounts(request: ListUkSavingsAccountRequest)(implicit
+      hc: HeaderCarrier,
+      ec: ExecutionContext,
+      logContext: EndpointLogContext,
+      correlationId: String): Future[Either[ErrorWrapper, ResponseWrapper[ListUkSavingsAccountResponse[UkSavingsAccount]]]] = {
+
+      EitherT(connector.listUkSavingsAccounts(request))
+        .leftMap(mapDesErrors(mappingDesToMtdError))
+        .value
+  }
+
+  private def mappingDesToMtdError: Map[String, MtdError] = Map(
+    "INVALID_ID_TYPE"          -> StandardDownstreamError,
+    "INVALID_IDVAlUE"          -> NinoFormatError,
+    "INVALID_INCOMESOURCETYPE" -> StandardDownstreamError,
+    "INVALID_TAXYEAR"          -> StandardDownstreamError, // Is tech spec correct here?
+    "INVALID_INCOMESOURCEID"   -> SavingsAccountIdFormatError,
+    "INVALID_ENDDATE"          -> StandardDownstreamError,
+    "NOT_FOUND"                -> NotFoundError,
+    "SERVER_ERROR"             -> StandardDownstreamError,
+    "SERVICE_UNAVAILABLE"      -> StandardDownstreamError
+  )
+
+}
