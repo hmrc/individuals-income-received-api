f24447bc25f143dd3e26c490f23ec692c07f33a6
Michael Olafisoye <molafisoye@gmail.com>
2022-10-02 23:14:43 +0100

    Revert "Revert "MTDSA-12901 changes to add new connector and service for the other employment income api""
    This reverts commit e04ad087ea433ee6ad49d9f20339cd3894d76148.

---
+++ app/v1/services/DeleteOtherEmploymentIncomeService.scala
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.services
+
+import api.connectors.DownstreamUri.DesUri
+import api.controllers.EndpointLogContext
+import api.models.domain.{Nino, TaxYear}
+import api.models.errors._
+import api.models.outcomes.ResponseWrapper
+import api.support.DownstreamResponseMappingSupport
+import cats.data.EitherT
+import javax.inject.{Inject, Singleton}
+import uk.gov.hmrc.http.HeaderCarrier
+import utils.Logging
+import v1.connectors.DeleteOtherEmploymentIncomeConnector
+
+import scala.concurrent.{ExecutionContext, Future}
+
+@Singleton
+class DeleteOtherEmploymentIncomeService @Inject() (connector: DeleteOtherEmploymentIncomeConnector)
+    extends DownstreamResponseMappingSupport
+    with Logging {
+
+  def delete(nino: Nino, taxYear: TaxYear)(implicit
+      desUri: DesUri[Unit],
+      hc: HeaderCarrier,
+      ec: ExecutionContext,
+      logContext: EndpointLogContext,
+      correlationId: String): Future[Either[ErrorWrapper, ResponseWrapper[Unit]]] = {
+
+    val result = for {
+      desResponseWrapper <- EitherT(connector.deleteOtherEmploymentIncome(nino, taxYear, desUri)).leftMap(mapDownstreamErrors(errorMap))
+    } yield desResponseWrapper
+
+    result.value
+  }
+
+  private def errorMap: Map[String, MtdError] = {
+    val errorMap = Map(
+      "INVALID_TAXABLE_ENTITY_ID" -> NinoFormatError,
+      "INVALID_TAX_YEAR"          -> TaxYearFormatError,
+      "INVALID_CORRELATIONID"     -> StandardDownstreamError,
+      "NO_DATA_FOUND"             -> NotFoundError,
+      "SERVER_ERROR"              -> StandardDownstreamError,
+      "SERVICE_UNAVAILABLE"       -> StandardDownstreamError
+    )
+
+    val extraTysErrors = Map(
+      "INVALID_TAX_YEAR"           -> TaxYearFormatError,
+      "INVALID_INCOMESOURCE_TYPE"  -> StandardDownstreamError,
+      "INVALID_CORRELATIONID"      -> StandardDownstreamError,
+      "TAX_YEAR_NOT_SUPPORTED"     -> RuleTaxYearNotSupportedError,
+      "INCOME_SOURCE_NOT_FOUND"    -> NotFoundError,
+      "INCOMPATIBLE_INCOME_SOURCE" -> StandardDownstreamError
+    )
+    errorMap ++ extraTysErrors
+  }
+
+}
