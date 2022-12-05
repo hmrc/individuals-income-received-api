b5d98ae5123086f8d4384defb76954894fa2b35a
Samuel Jacobs <108878134+SamuelJacobsHMRC@users.noreply.github.com>
2022-12-05 14:32:41 +0000

    MTDSA-13824: Implemented TYS to ConnectorSpec and ServiceSpec

--- test/v1/connectors/AmendOtherEmploymentConnectorSpec.scala
+++ test/v1/connectors/AmendOtherEmploymentConnectorSpec.scala
@@ -17,183 +17,92 @@
 package v1.connectors
 
 import api.connectors.ConnectorSpec
-import api.mocks.MockHttpClient
 import api.models.domain.{Nino, TaxYear}
+import api.models.errors.{NinoFormatError, StandardDownstreamError}
 import api.models.outcomes.ResponseWrapper
-import mocks.MockAppConfig
-import uk.gov.hmrc.http.HeaderCarrier
 import v1.models.request.amendOtherEmployment._
 
 import scala.concurrent.Future
 
 class AmendOtherEmploymentConnectorSpec extends ConnectorSpec {
 
-  private val nino: String    = "AA111111A"
-  private val taxYear: String = "2019-20"
+  "AmendOtherEmploymentConnector" should {
+    "return the expected response for a non-TYS request" when {
+      "a valid request is made" in new DesTest with Test {
+        def taxYear: TaxYear = TaxYear.fromMtd("2019-20")
+        val outcome          = Right(ResponseWrapper(correlationId, ()))
 
-  private val shareOptionModel = Seq(
-    AmendShareOptionItem(
-      employerName = "Company Ltd",
-      employerRef = Some("AB1321/123"),
-      schemePlanType = "EMI",
-      dateOfOptionGrant = "2019-11-20",
-      dateOfEvent = "2019-12-22",
-      optionNotExercisedButConsiderationReceived = true,
-      amountOfConsiderationReceived = 23122.22,
-      noOfSharesAcquired = 1,
-      classOfSharesAcquired = "FIRST",
-      exercisePrice = 12.22,
-      amountPaidForOption = 123.22,
-      marketValueOfSharesOnExcise = 1232.22,
-      profitOnOptionExercised = 1232.33,
-      employersNicPaid = 2312.22,
-      taxableAmount = 2132.22
-    ),
-    AmendShareOptionItem(
-      employerName = "Corp Ltd",
-      employerRef = Some("AB1321/345"),
-      schemePlanType = "CSOP",
-      dateOfOptionGrant = "2019-09-10",
-      dateOfEvent = "2019-07-29",
-      optionNotExercisedButConsiderationReceived = false,
-      amountOfConsiderationReceived = 30000.22,
-      noOfSharesAcquired = 5,
-      classOfSharesAcquired = "SECOND",
-      exercisePrice = 10.50,
-      amountPaidForOption = 150.50,
-      marketValueOfSharesOnExcise = 2250.22,
-      profitOnOptionExercised = 1350.40,
-      employersNicPaid = 2450.25,
-      taxableAmount = 2140.20
-    )
-  )
+        willPut(
+          url = s"$baseUrl/income-tax/income/other/employments/$nino/${taxYear.asMtd}",
+          body = amendOtherEmploymentRequestBody
+        ).returns(Future.successful(outcome))
 
-  private val sharesAwardedOrReceivedModel = Seq(
-    AmendSharesAwardedOrReceivedItem(
-      employerName = "Company Ltd",
-      employerRef = Some("AB1321/123"),
-      schemePlanType = "SIP",
-      dateSharesCeasedToBeSubjectToPlan = "2019-11-10",
-      noOfShareSecuritiesAwarded = 11,
-      classOfShareAwarded = "FIRST",
-      dateSharesAwarded = "2019-12-20",
-      sharesSubjectToRestrictions = true,
-      electionEnteredIgnoreRestrictions = false,
-      actualMarketValueOfSharesOnAward = 2123.22,
-      unrestrictedMarketValueOfSharesOnAward = 123.22,
-      amountPaidForSharesOnAward = 123.22,
-      marketValueAfterRestrictionsLifted = 1232.22,
-      taxableAmount = 12321.22
-    ),
-    AmendSharesAwardedOrReceivedItem(
-      employerName = "Corp Ltd",
-      employerRef = Some("AB1326/789"),
-      schemePlanType = "Other",
-      dateSharesCeasedToBeSubjectToPlan = "2019-10-25",
-      noOfShareSecuritiesAwarded = 15,
-      classOfShareAwarded = "SECOND",
-      dateSharesAwarded = "2019-08-23",
-      sharesSubjectToRestrictions = true,
-      electionEnteredIgnoreRestrictions = false,
-      actualMarketValueOfSharesOnAward = 2140.23,
-      unrestrictedMarketValueOfSharesOnAward = 125.30,
-      amountPaidForSharesOnAward = 124.25,
-      marketValueAfterRestrictionsLifted = 1259.40,
-      taxableAmount = 12450.30
-    )
-  )
+        await(connector.amendOtherEmployment(amendOtherEmploymentRequest)) shouldBe outcome
+      }
 
-  private val disabilityModel = AmendCommonOtherEmployment(
-    customerReference = Some("customer reference"),
-    amountDeducted = 1223.22
-  )
+      "downstream returns a single error" in new DesTest with Test {
+        def taxYear: TaxYear = TaxYear.fromMtd("2019-20")
 
-  private val foreignServiceModel = AmendCommonOtherEmployment(
-    customerReference = Some("cust ref"),
-    amountDeducted = 1234.50
-  )
+        val outcome = Left(ResponseWrapper(correlationId, NinoFormatError))
 
-  private val taxableLumpSumsAndCertainIncome = AmendTaxableLumpSumsAndCertainIncomeItem(
-    amount = 5000.99,
-    taxPaid = Some(3333.33),
-    taxTakenOffInEmployment = true
-  )
+        willPut(
+          url = s"$baseUrl/income-tax/income/other/employments/$nino/${taxYear.asMtd}",
+          body = amendOtherEmploymentRequestBody
+        ).returns(Future.successful(outcome))
 
-  private val benefitFromEmployerFinancedRetirementScheme = AmendBenefitFromEmployerFinancedRetirementSchemeItem(
-    amount = 5000.99,
-    exemptAmount = Some(2345.99),
-    taxPaid = Some(3333.33),
-    taxTakenOffInEmployment = true
-  )
+        await(connector.amendOtherEmployment(amendOtherEmploymentRequest)) shouldBe outcome
+      }
 
-  private val redundancyCompensationPaymentsOverExemption = AmendRedundancyCompensationPaymentsOverExemptionItem(
-    amount = 5000.99,
-    taxPaid = Some(3333.33),
-    taxTakenOffInEmployment = true
-  )
+      "downstream returns multiple errors" in new DesTest with Test {
+        def taxYear: TaxYear = TaxYear.fromMtd("2019-20")
 
-  private val redundancyCompensationPaymentsUnderExemption = AmendRedundancyCompensationPaymentsUnderExemptionItem(
-    amount = 5000.99
-  )
+        val outcome = Left(ResponseWrapper(correlationId, Seq(NinoFormatError, StandardDownstreamError)))
 
-  private val lumpSums = Seq(
-    AmendLumpSums(
-      employerName = "BPDTS Ltd",
-      employerRef = "123/AB456",
-      taxableLumpSumsAndCertainIncome = Some(taxableLumpSumsAndCertainIncome),
-      benefitFromEmployerFinancedRetirementScheme = Some(benefitFromEmployerFinancedRetirementScheme),
-      redundancyCompensationPaymentsOverExemption = Some(redundancyCompensationPaymentsOverExemption),
-      redundancyCompensationPaymentsUnderExemption = Some(redundancyCompensationPaymentsUnderExemption)
-    )
-  )
+        willPut(
+          url = s"$baseUrl/income-tax/income/other/employments/$nino/${taxYear.asMtd}",
+          body = amendOtherEmploymentRequestBody
+        ).returns(Future.successful(outcome))
 
-  private val amendOtherEmploymentRequestBody = AmendOtherEmploymentRequestBody(
-    shareOption = Some(shareOptionModel),
-    sharesAwardedOrReceived = Some(sharesAwardedOrReceivedModel),
-    disability = Some(disabilityModel),
-    foreignService = Some(foreignServiceModel),
-    lumpSums = Some(lumpSums)
-  )
+        await(connector.amendOtherEmployment(amendOtherEmploymentRequest)) shouldBe outcome
+      }
+    }
+    "return the expected response for a TYS request" when {
+      "a valid request is made" in new TysIfsTest with Test {
+        def taxYear: TaxYear = TaxYear.fromMtd("2023-24")
+        val outcome          = Right(ResponseWrapper(correlationId, ()))
 
-  private val amendOtherEmploymentRequest = AmendOtherEmploymentRequest(
-    nino = Nino(nino),
-    taxYear = TaxYear.fromMtd(taxYear),
-    body = amendOtherEmploymentRequestBody
-  )
+        willPut(
+          url = s"$baseUrl/income-tax/income/other/employments/${taxYear.asTysDownstream}/$nino",
+          body = amendOtherEmploymentRequestBody
+        ).returns(Future.successful(outcome))
 
-  class Test extends MockHttpClient with MockAppConfig {
+        await(connector.amendOtherEmployment(amendOtherEmploymentRequest)) shouldBe outcome
+      }
+    }
+  }
+
+  trait Test {
+    _: ConnectorTest =>
+
+    def taxYear: TaxYear
+
+    protected val nino: String = "AA111111A"
+
+    protected val amendOtherEmploymentRequestBody: AmendOtherEmploymentRequestBody =
+      AmendOtherEmploymentRequestBody(None, None, None, None, None)
+
+    protected val amendOtherEmploymentRequest: AmendOtherEmploymentRequest =
+      AmendOtherEmploymentRequest(
+        nino = Nino(nino),
+        taxYear = taxYear,
+        body = amendOtherEmploymentRequestBody
+      )
 
     val connector: AmendOtherEmploymentConnector = new AmendOtherEmploymentConnector(
       http = mockHttpClient,
       appConfig = mockAppConfig
     )
 
-    MockedAppConfig.desBaseUrl returns baseUrl
-    MockedAppConfig.desToken returns "des-token"
-    MockedAppConfig.desEnvironment returns "des-environment"
-    MockedAppConfig.desEnvironmentHeaders returns Some(allowedDesHeaders)
-  }
-
-  "AmendOtherEmploymentConnector" when {
-    "amendOtherEmployment" must {
-      "return a 204 status for a success scenario" in new Test {
-        val outcome                                      = Right(ResponseWrapper(correlationId, ()))
-        implicit val hc: HeaderCarrier                   = HeaderCarrier(otherHeaders = otherHeaders ++ Seq("Content-Type" -> "application/json"))
-        val requiredDesHeadersPut: Seq[(String, String)] = requiredDesHeaders ++ Seq("Content-Type" -> "application/json")
-
-        MockedHttpClient
-          .put(
-            url = s"$baseUrl/income-tax/income/other/employments/$nino/$taxYear",
-            config = dummyDesHeaderCarrierConfig,
-            body = amendOtherEmploymentRequestBody,
-            requiredHeaders = requiredDesHeadersPut,
-            excludedHeaders = Seq("AnotherHeader" -> "HeaderValue")
-          )
-          .returns(Future.successful(outcome))
-
-        await(connector.amendOtherEmployment(amendOtherEmploymentRequest)) shouldBe outcome
-      }
-    }
   }
 
 }
