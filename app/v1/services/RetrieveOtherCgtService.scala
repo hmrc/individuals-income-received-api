eef7146e0ebd3018dbafa786a8771fc3519f7608
GazalNebHMRC <108878679+GazalNebHMRC@users.noreply.github.com>
2022-12-12 05:39:50 +0000

    MTDSA-13889: TYS changes for Retrieve Other Capital Gains and Disposals

---
+++ app/v1/services/RetrieveOtherCgtService.scala
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.services
+
+import api.controllers.EndpointLogContext
+import api.models.errors._
+import api.models.outcomes.ResponseWrapper
+import api.support.DownstreamResponseMappingSupport
+import cats.data.EitherT
+import cats.implicits._
+import uk.gov.hmrc.http.HeaderCarrier
+import utils.Logging
+import v1.connectors.RetrieveOtherCgtConnector
+import v1.models.request.retrieveOtherCgt.RetrieveOtherCgtRequest
+import v1.models.response.retrieveOtherCgt.RetrieveOtherCgtResponse
+
+import javax.inject.{Inject, Singleton}
+import scala.concurrent.{ExecutionContext, Future}
+
+@Singleton
+class RetrieveOtherCgtService @Inject() (connector: RetrieveOtherCgtConnector) extends DownstreamResponseMappingSupport with Logging {
+
+  def retrieve(request: RetrieveOtherCgtRequest)(implicit
+      hc: HeaderCarrier,
+      ec: ExecutionContext,
+      logContext: EndpointLogContext,
+      correlationId: String): Future[Either[ErrorWrapper, ResponseWrapper[RetrieveOtherCgtResponse]]] = {
+
+    val result = EitherT(connector.retrieveOtherCgt(request)).leftMap(mapDownstreamErrors(downstreamErrorMap))
+
+    result.value
+  }
+
+  private def downstreamErrorMap: Map[String, MtdError] = {
+    val errors = Map(
+      "INVALID_TAXABLE_ENTITY_ID" -> NinoFormatError,
+      "INVALID_TAX_YEAR"          -> TaxYearFormatError,
+      "INVALID_CORRELATIONID"     -> StandardDownstreamError,
+      "NO_DATA_FOUND"             -> NotFoundError,
+      "SERVER_ERROR"              -> StandardDownstreamError,
+      "SERVICE_UNAVAILABLE"       -> StandardDownstreamError
+    )
+
+    val extraTysErrors = Map(
+      "INVALID_CORRELATION_ID" -> StandardDownstreamError,
+      "TAX_YEAR_NOT_SUPPORTED" -> RuleTaxYearNotSupportedError
+    )
+
+    errors ++ extraTysErrors
+  }
+
+}
