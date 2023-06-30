49e263ce5f9558da93a2bbc519f9525b675bd1fc
chetan-jain-atos <112093269+chetan-jain-atos@users.noreply.github.com>
2023-06-30 13:59:18 +0100

    MTDSA-17472

--- app/api/models/errors/CommonMtdErrors.scala
+++ app/api/models/errors/CommonMtdErrors.scala
@@ -67,6 +67,9 @@ object SavingsAccountIdFormatError
 object RuleTaxYearNotSupportedError
     extends MtdError("RULE_TAX_YEAR_NOT_SUPPORTED", "The tax year specified does not lie within the supported range", BAD_REQUEST)
 
+object RuleInvalidSubmissionPensionSchemeError
+    extends MtdError("RULE_INVALID_SUBMISSION_PENSION_SCHEME", "The remote endpoint has indicated that Invalid Data item submitted for Occupational Pension Scheme", BAD_REQUEST)
+
 object RuleIncorrectOrEmptyBodyError
     extends MtdError("RULE_INCORRECT_OR_EMPTY_BODY_SUBMITTED", "An empty or non-matching body was submitted", BAD_REQUEST)
 
